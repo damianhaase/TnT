751d12e010dd88e240a5cc9dd0510521934823c7
Damian Haase <damianhaase@gmail.com>
2011-06-01 00:48:36 -0400

    Added README and working to make multiple character selection

--- combat1.py
+++ combat1.py
@@ -26,20 +26,54 @@ char2_weaponAdds = round(char2_MR / 2, 0)
 ######################################
 
 
-# display
+# set up sides
+######################################
+
+# list of attributes:
+# name str
+# weapon str
+# weapon_dice int
+# weapon adds int
+# personal adds
+# CN int
+
+charList = [char1, char2, char3];
+
+char1 = { 'name' : 'Barry', 'weapon' : 'Sabre', 'weapon_dice' : 3, 'weapon_adds' : 4, 'personal_adds': 4, 'CN' : 18 };
+char2 = { 'name' : 'Manticore', 'weapon' : 'Claws', 'weapon_dice' : 2, 'weapon_adds' : 0, 'personal_adds': 8, 'CN' : 18 };
+char3 = { 'name' : 'Six Pack', 'weapon' : 'Fists', 'weapon_dice' : 3, 'weapon_adds' : 0, 'personal_adds': 4, 'CN' : 24 };
+
+
+print "Set sides. There are %d characters to allocate." % len(charList)
+
+print "1: %s, %d personal adds, using %s, %dd6+%d, %d CN" % ( char1['name'], char1['personal_adds'], char1['weapon'], char1['weapon_dice'], char1['weapon_adds'], char1['CN'] )
+print "2: %s, %d personal adds, using %s, %dd6+%d, %d CN" % ( char2['name'], char2['personal_adds'], char2['weapon'], char2['weapon_dice'], char2['weapon_adds'], char2['CN'] )
+print "3: %s, %d personal adds, using %s, %dd6+%d, %d CN" % ( char3['name'], char3['personal_adds'], char3['weapon'], char3['weapon_dice'], char3['weapon_adds'], char3['CN'] )
+
+# make some sides
+aSide = [];
+bSide = [];
+
+for character in charList:
+    currentName = character['name'];
+    char1_sideinput = raw_input ("%s, side a or b? " % currentName)
+    if (char1_sideinput == 'a'):
+        aSide.append ( char1 ); 
+        print "side a it is!"
+    elif (char1_sideinput == 'b'):
+        print "okay, side b!"
+
+######################################
+
+
+
+# combat results display
 ######################################
 
 print "This will simulate multiple combat rounds between the following characters:"
-print "%s, Human, %d personal adds" % (char1_name, char1_persAdds)
-print "%d HP" % char1_CN
-print "using %s %dD6+%d" % (char1_weapon, char1_weaponDice, char1_weaponAdds)
-
+print "%s, Human, %d personal adds, %d HP, using %s %dD6+%d" % (char1_name, char1_persAdds, char1_CN, char1_weapon, char1_weaponDice, char1_weaponAdds)
 print "vs."
-
-print "%s" % char2_name
-print "MR %d, %dD6+%d" % (char2_MR, char2_weaponDice, char2_weaponAdds)
-print "%d HP" % char2_MR
-
+print "%s, MR %d, %dD6+%d, %d HP" % (char2_name, char2_MR, char2_weaponDice, char2_weaponAdds, char2_MR)
 
 # make copies of CN and MR to decrement
 char1_HP = char1_CN
@@ -48,8 +82,6 @@ char2_HP = char2_MR
 # while either character's HP > 0, run the combat round loop
 round = 1
 while (char1_HP > 0  and char2_HP > 0):
-    print "Combat round %d:" % round    
-    round = round + 1
     char1_roll = dice_roller.multiDie( char1_weaponDice , 1 )
     char1_total = char1_roll + char1_weaponAdds
     char2_roll = dice_roller.multiDie( char2_weaponDice , 1 )
@@ -58,17 +90,18 @@ while (char1_HP > 0  and char2_HP > 0):
     #print "%s rolled %d + %d adds" % (char2_name , char2_roll, char2_weaponAdds)
     
     if char1_total == char2_total:
-        print "This round was a draw."
+        print "%d    draw" % round
     elif char1_total > char2_total:
-        print "%s wins this round, doing %d points of damage to %s" % (char1_name, char1_total - char2_total, char2_name)
         char2_HP = char2_HP - (char1_total - char2_total)
-        print "%s now has %d HP" % (char2_name, char2_HP)
+        if (char2_HP <= 0):
+            print "%d    %s is dead" % (round, char2_name)
+            break
+        print "%d    %s %d" % (round, char2_name, char2_HP)
     elif char1_total < char2_total:
-        print "%s wins this round, doing %d points of damage to %s" % (char2_name, char2_total - char1_total, char1_name)               
         char1_HP = char1_HP - (char2_total - char1_total)
-        print "%s now has %d HP" % (char1_name, char1_HP)
-    
-if (char1_HP <= 0):
-    print "%s is dead" % char1_name
-elif (char2_HP <= 0):
-    print "%s is dead" % char2_name
+        if (char1_HP <= 0):
+            print "%d    %s is dead" % (round, char1_name)
+            break
+        print "%d    %s %d" % (round, char1_name, char1_HP)
+    round = round + 1
+######################################
